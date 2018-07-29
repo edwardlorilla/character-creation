a37c67ff776cec774b87eaebc69d02192ed8a839
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2018-07-30 02:36:35 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+new Vue({
+    data(){
+        return {
+            glassNumber: 1,
+            hatNumber: 1,
+            mouthNumber: 1,
+            eyeNumber: 1,
+            hairNumber: 1,
+            hornNumber: 1,
+            bodyNumber: 1,
+            armNumber: 1,
+            legNumber: 1,
+        }
+    }
+}).$mount('#app')
